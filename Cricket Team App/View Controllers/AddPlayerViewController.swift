defcfad49bb4ccc7a8c9d92e33e1daeefe406597
Ajinkya Sonar <Ajinkya@ajinkyas-macbook-pro.local>
2018-09-10 01:11:40 +0530

    App Layout Complete with View Controllers

---
+++ Cricket Team App/View Controllers/AddPlayerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddPlayerViewController.swift
+//  Cricket Team App
+//
+//  Created by Ajinkya Sonar on 10/09/18.
+//  Copyright © 2018 Ajinkya Sonar. All rights reserved.
+//
+
+import UIKit
+
+class AddPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
---
+++ Cricket Team App/View Controllers/PlayerManagementViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PlayerManagementViewController.swift
+//  Cricket Team App
+//
+//  Created by Ajinkya Sonar on 10/09/18.
+//  Copyright © 2018 Ajinkya Sonar. All rights reserved.
+//
+
+import UIKit
+
+class PlayerManagementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
